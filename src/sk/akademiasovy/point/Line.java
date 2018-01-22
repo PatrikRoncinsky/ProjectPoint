696c80c702aad2cc57fb97a637b85f9fe80ed4a4
PatrikRoncinsky <p.roncinsky@gmail.com>
2018-01-22 13:22:12 +0100

    Initial commit

---
+++ src/sk/akademiasovy/point/Line.java
@@ -0,0 +1,32 @@
+package sk.akademiasovy.point;
+
+public class Line {
+    private Point a;
+    private Point b;
+
+    public Line(Point a, Point b){
+        this.a=a;
+        this.b=b;
+    }
+
+    public Line(int xa, int ya, int xb, int yb){
+        a=new Point(xa,xb);
+        b=new Point(xb,yb);
+    }
+
+    public double getLength(){
+        return a.getDistanceFromOtherPoint(b);
+    }
+
+    public Point getMiddlePointOfLine(){
+        return new Point((a.getX()+b.getX())/2,(a.getY()+b.getY())/2);
+    }
+
+    public boolean isParallelToTheAxisX(){
+        return a.getY()==b.getY();
+    }
+
+    public boolean isParallel1ToTheAxisY(){
+        return a.getX()==b.getX();
+    }
+}
